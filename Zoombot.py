06ebd227597b59cb09d187d5d50d83b7ae38e0be
InstructorElvis <82903015+InstructorElvis@users.noreply.github.com>
2021-06-15 17:06:51 -0400

    Removed Metadata and Included Variables
    Replaced some zoom links that were linked to real rooms with placeholder variables that will take in the value of the "invitation" links. They could be from Zoom, Google Meet, etc.

--- Zoombot.py
+++ Zoombot.py
@@ -8,12 +8,14 @@ import schedule
 import time
 import webbrowser
 
+#Pass in any link to this function to have it open that link on the webbrowser.
 def open_link(link):
     webbrowser.open(link)
 
 
 def intro_2_python():
-    webbrowser.open("https://dfa.zoom.us/j/96815350328")
+    intro2Python = ""
+    webbrowser.open(f"{intro2Python}")
 
 def aws_cloud_foundations():
     #Zoom Invite Link goes here: 
@@ -22,8 +24,8 @@ def aws_cloud_foundations():
     #This will open your Zoom Invite Link
     webbrowser.open(f"{awsZoomLink}")
 
-def elvis_room():
-    webbrowser.open("https://us04web.zoom.us/j/2881787457?pwd=aUZHWlJncS8vL1FoTXlUcWJWaGN4UT09")
+    
+#Use the schedule module in order to leverage its method to schedule your function to run at a certain time. 
 
 schedule.every().monday.at("10:30").do(aws_cloud_foundations)
 schedule.every().tuesday.at("10:30").do(aws_cloud_foundations)
@@ -31,6 +33,7 @@ schedule.every().wednesday.at("10:30").do(aws_cloud_foundations)
 schedule.every().thursday.at("10:30").do(aws_cloud_foundations)
 
 #What am I gonna do if it's not the scheduled time?
+#Create an infinite loop that will run until the time is met. 
 while True:
     schedule.run_pending()
     print("Not Yet...")
